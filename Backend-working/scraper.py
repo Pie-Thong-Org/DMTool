dbd34c37a6fe1d979d6e72373c08f61c103fafa6
t-btay <137745300+t-btay@users.noreply.github.com>
2023-08-26 12:54:24 -0700

    Add files via upload
    Created proper functions. No changes to actual runtime code.
    Functions haven't been tested.

--- Backend-working/scraper.py
+++ Backend-working/scraper.py
@@ -20,6 +20,7 @@ dndhomepage = requests.get("http://dnd5e.wikidot.com")
 soup = BSoup(dndhomepage.text, "lxml")
 
 links = []
+
 hpage_navlist = ["Weapons"]
 weapontable_navlist = ["Simple Weapons"]
 ###############################################################################
@@ -30,6 +31,126 @@ weapontable_navlist = ["Simple Weapons"]
 
 #functions
 
+def navigate_site_level(orig_url, page_navlist, *links):
+    '''
+    Parameters
+    ----------
+    orig_url 
+        Type: Str
+        Desc: URL of page to be searched.
+    page_navlist 
+        Type: List
+        Desc: List of search keywords. Hyperlinks containing these strings will be returned as output.
+    *links 
+        Type: List
+        Desc: Optional parameter to provide additional specific urls to navigate through. This list will be appended with
+        search results from page_navlist.
+
+    Returns: List of new url strings
+    -------
+    Searches the given url page for hyperlinks that contain given keyword strings.
+    Used to navigate the current site level through hyperlinks. The url's returned depend on the keywords
+    provided in the page_navlist parameter.
+    
+    Notes:
+        -Needs testing
+        -Add error handling loops and feedback messages
+    '''
+    
+    new_urls = []
+    
+    if not links:
+        links = []
+        
+    get_links = soup.find_all("a")
+
+    for link in get_links:
+        if page_navlist:
+            if link.string in page_navlist:
+                links.append(link)
+        else:
+            break
+ 
+    for new_link in links:
+        new_urls = requests.get(f"{orig_url}{links[i]['href']}")
+        new_urls.append(new_link)
+        print(new_link)
+    
+    return new_urls
+
+def scrape_url(nav_page, *data_org, number_of_headers):
+    '''
+    
+    Parameters
+    ----------
+    nav_page : TYPE
+        DESCRIPTION.
+    *data_org : TYPE
+        DESCRIPTION.
+    number_of_headers : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    data_dict : TYPE
+        DESCRIPTION.
+
+    '''
+    data_list = []
+    data_dict = {}
+    
+    if not data_org:
+        data_org = "table"
+    
+    if not number_of_headers:
+        number_of_headers = 5
+        
+    nav_soup = BSoup(nav_page.text, "lxml")
+    print(f"URL Return: \n {nav_soup}")
+    
+    page_tables = nav_soup.find_all("table")
+
+    for table in page_tables:
+        cell = table.find_all("td")
+        for data in cell:
+            data_list.append(data.string)
+
+    listpack = []
+    counter = 0
+    for i in range(number_of_headers-1,len(data_list), number_of_headers):
+        
+        number_iter = number_of_headers
+        for j in range(number_of_headers):
+            listpack.append([number_iter+1])
+            number_iter -= 1
+            
+        data_dict[f"{counter+1}"] = listpack
+        counter += 1
+       
+        return data_dict
+
+def create_json(python_dict, file_to_write):
+    '''
+
+    Parameters
+    ----------
+    python_dict : TYPE
+        DESCRIPTION.
+    file_to_write : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    jsconvert = json.dumps(python_dict, indent = 1)
+    
+    with open(file_to_write, "w") as file:
+        file.write(jsconvert)
+    
+    print("file written")
+    
 ###############################################################################
 
 get_links = soup.find_all("a")
