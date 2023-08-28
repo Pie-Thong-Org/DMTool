c381fa795aaaec4e764ca885ceaa0c64d065fc80
t-btay <137745300+t-btay@users.noreply.github.com>
2023-08-28 08:19:38 -0700

    Add files via upload
    db connection working, using certifi cert. still testing pymongo functionality

--- Backend-working/DBAccess.py
+++ Backend-working/DBAccess.py
@@ -6,19 +6,43 @@ Created on Sun Aug 27 15:22:12 2023
 """
 
 from pymongo import MongoClient
+import certifi
+ 
 
 user = "t-btay"
 passw = "jgAXBgHB6RNcGCP5"
-connection_string = f"mongodb+srv://{user}:{passw}@cluster0.mfbl8ws.mongodb.net/?retryWrites=true&w=majority"
+ca = certifi.where()
+connection_string = f"mongodb+srv://{user}:{passw}@cluster0?ssl=true&ssl_cert_reqs=CERT_NONE.mfbl8ws.mongodb.net/?retryWrites=true&w=majority"
 
 
+###############################################################################
 
-cluster = MongoClient(connection_string)
+#functions
+
+def ping_db_server(db):
+    print("pinging...")
+    db.admin.command("ping")
+    print("Connection successful")
+
+###############################################################################
+
+
+cluster = MongoClient(connection_string, tlsCAFile = ca)
+ping_db_server(cluster)
 database = cluster["TestDB"]
 collection = database["TestColl"]
 
-post = {"_id":0, "name":"DBTestName", "score": 5}
+post1 = {"_id":0, "name":"tony", "score": 5}
+post2 = {"_id":1, "name":"dan", "score": 4}#lol
 
-collection.insert_one(post)
+#collection.update_one({"name":"jim"},{"$inc":{"hats":1}})
+
+post_count = collection.count_documents({})
+print(f"posts: {post_count} \n")
+
+results = collection.find({})
+for i in results:
+    print(i)
+print("finished")
 
 cluster.close()
